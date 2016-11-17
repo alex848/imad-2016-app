4522deae6e62eb7ed0d537b81494fd0f395f751b
alex848 <alokmishra848@gmail.com>
2016-11-17 23:36:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,12 +4,69 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleonre={
+    title:'Article-one!Alok Mishra',
+    heading:'Article-one',
+    date:'Sep 15 2016',
+    content:`/<p>
+            This is the content for my first Article.This is the content for my first Article.
+            This is the content for my first Article.
+            This is the content for my first Article.
+            This is the content for my first Article.
+            This is the content for my first Article.
+        </p>
+        <p>
+            This is the content for my first Article.
+            This is the content for my first Article.
+            This is the content for my first Article.
+            This is the content for my first Article.
+            
+    </p>`
+};
+Function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+var htmltemplate=`
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <style>
+           <link href="/ui/style.css" rel="stylesheet" />
+        </style>
+    </head>
+    <body>
+        <div class ="container">
+            
+        <div>
+        <a href="/">Home</a>
+        </div>
+        <hr>
+        <h1>${heading}</h1>
+        <div>
+            Nov, 17 2016
+        </div>
+        <div>
+        ${date}
+        ${content}
+        </div>
+    </body>
+    
+
+</html
+`;
+return htmltemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.sendFile(createTemplate(articleOne));
 });
 app.get('/article-two', function (req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
